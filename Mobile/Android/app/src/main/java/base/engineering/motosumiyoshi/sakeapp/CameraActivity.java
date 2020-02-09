637ae365245ce2250c08829715566f4b402a0b6a
yitakura731 <yitakura731@gmail.com>
2020-02-09 16:05:49 +0900

    Camera Intentから返されるカメラ画像サイズが大きくなるように修正

--- Mobile/Android/app/src/main/java/base/engineering/motosumiyoshi/sakeapp/CameraActivity.java
+++ Mobile/Android/app/src/main/java/base/engineering/motosumiyoshi/sakeapp/CameraActivity.java
@@ -1,32 +1,67 @@
 package base.engineering.motosumiyoshi.sakeapp;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class CameraActivity extends AppCompatActivity {
 
+    private Uri imageUri = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
     }
 
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 200 && resultCode == RESULT_OK) {
-            Bitmap bitmap = data.getParcelableExtra("data");
             ImageView ivCamera = findViewById(R.id.ivCamera);
-            ivCamera.setImageBitmap(bitmap);
+            ivCamera.setImageURI(imageUri);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 200 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            ImageView ivCamera = findViewById(R.id.ivCamera);
+            onCameraImageClick(ivCamera);
         }
     }
 
     public void onCameraImageClick(View view) {
-        startActivityForResult(
-                new Intent(MediaStore.ACTION_IMAGE_CAPTURE),
-                200);
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    2000);
+            return;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+        String fileName = "nonbei_img_" + sdf.format(new Date()) + ".jpg";
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, fileName);
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        ContentResolver cr = getContentResolver();
+        this.imageUri = cr.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, 200);
     }
 }
